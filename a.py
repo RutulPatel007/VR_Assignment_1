73704f00563f25af75067c98c77f366f7f6f70bb
Rutul Patel <githubrp@gmail.com>
2025-03-02 21:46:09 +0530

    final

--- a.py
+++ a.py
@@ -1,4 +1,10 @@
 from VR_Assignment_Rutul.part1.coinDetection import pipeline
 
-pipeline('test_coins/0.jpg', 'output')
+from VR_Assignment_Rutul.part2.panorama import panorama
+
+
+for i in range(0,16):
+    pipeline(f"coins/{i}.jpg", f"output_coin_detection_{i}")
+for i in range(1,6):
+    panorama(f"input{i}", f"output_panorama_{i}")
 
